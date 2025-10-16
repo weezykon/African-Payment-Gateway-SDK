2d78c8fd2bbe4340fcd5a6dd88b85f89317bfd2d
Akinbode Akinlolu <weezykon@gmail.com>
2025-10-16 10:44:33 +0100

    fix: Correct ESLint flat config imports and setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslintParser from "@typescript-eslint/parser";
+import tseslintPlugin from "@typescript-eslint/eslint-plugin";
 import prettierConfig from "eslint-config-prettier";
 
 export default [
   {
     languageOptions: {
       globals: globals.node,
+      parser: tseslintParser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslintPlugin,
     },
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslintPlugin.configs.recommended,
   prettierConfig,
   {
     files: ["**/*.ts"],
