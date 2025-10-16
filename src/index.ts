e45a4916fa2ed2e4fb74a1c9cbe00ee5250906f7
Akinbode Akinlolu <weezykon@gmail.com>
2025-10-16 09:29:02 +0100

    feat: Implement AfricanPaymentGatewaySDK class

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { PaymentGateway } from "./gateways";
+import { PaystackGateway } from "./gateways/paystack";
+import { FlutterwaveGateway } from "./gateways/flutterwave";
+
+export class AfricanPaymentGatewaySDK {
+  private gateway: PaymentGateway;
+
+  constructor(gatewayType: 'paystack' | 'flutterwave', credentials: any) {
+    if (gatewayType === 'paystack') {
+      this.gateway = new PaystackGateway(credentials.secretKey);
+    } else if (gatewayType === 'flutterwave') {
+      this.gateway = new FlutterwaveGateway(credentials.publicKey, credentials.secretKey);
+    } else {
+      throw new Error('Invalid gateway type provided.');
+    }
+  }
+
+  public initiateTransaction(amount: number, currency: string, customerEmail: string, reference: string): Promise<any> {
+    return this.gateway.initiateTransaction(amount, currency, customerEmail, reference);
+  }
+
+  public verifyTransaction(reference: string): Promise<any> {
+    return this.gateway.verifyTransaction(reference);
+  }
+}
