bb01c1b780744eaf8ba74bffe87a3503e2b7e120
Akinbode Akinlolu <weezykon@gmail.com>
2025-10-16 10:44:08 +0100

    chore: Update .gitignore and add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ["**/*.ts"],
+    rules: {
+      // Add any specific rules or overrides here
+    },
+  },
+];
