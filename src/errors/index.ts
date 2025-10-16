cb0f72e7efed774e19277d6fd56ba27e1f62553d
Akinbode Akinlolu <weezykon@gmail.com>
2025-10-16 09:31:06 +0100

    feat: Add custom error classes

---
+++ src/errors/index.ts
@@ -0,0 +1,20 @@
+export class PaymentGatewayError extends Error {
+  constructor(message: string, public code?: string) {
+    super(message);
+    this.name = 'PaymentGatewayError';
+  }
+}
+
+export class TransactionVerificationError extends PaymentGatewayError {
+  constructor(message: string, code?: string) {
+    super(message, code);
+    this.name = 'TransactionVerificationError';
+  }
+}
+
+export class WebhookVerificationError extends PaymentGatewayError {
+  constructor(message: string, code?: string) {
+    super(message, code);
+    this.name = 'WebhookVerificationError';
+  }
+}
